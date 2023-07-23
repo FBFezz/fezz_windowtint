bfa838066afd05e6b75b56d5b01650dca041dc37
Fezz <43589668+FBFezz@users.noreply.github.com>
2023-07-23 12:31:56 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,3 +4,4 @@ Config.AllowEveryone = true -- Enable to allow everyone to use the item / comman
 Config.EnableCommand = true -- Enable or Disable the checktint command
 Config.Target = false -- Enable to use ox_target
 Config.Item = false -- Enable if you use the window tint as an item
+Config.Animation = true -- Enable to show animation

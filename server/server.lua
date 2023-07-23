b8d29f3aa7fc78a9e6c6982aa00846a3821b1b80
Fezz <43589668+FBFezz@users.noreply.github.com>
2023-07-23 12:11:50 +0100

    Added Get Item Amount

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,10 @@
 ESX.RegisterUsableItem('tintchecker', function(source)
     TriggerClientEvent("fezz_windowtint:checkTint", source)
 end)
+
+ESX.RegisterServerCallback('fezz_windowtint:getItemAmount', function(source, cb, item)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local quantity = xPlayer.getInventoryItem(item).count
+
+	cb(quantity)
+end)

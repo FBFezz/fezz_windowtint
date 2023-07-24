befe70903726f6c626bb49e017f66b99e92735e9
Fezz <43589668+FBFezz@users.noreply.github.com>
2023-07-25 00:17:00 +0100

    Added multiple job support

--- config.lua
+++ config.lua
@@ -5,3 +5,7 @@ Config.EnableCommand = true -- Enable or Disable the checktint command
 Config.Target = false -- Enable to use ox_target
 Config.Item = false -- Enable if you use the window tint as an item
 Config.Animation = true -- Enable to show animation
+Config.policeJobs = { -- Police jobs 
+    'police'-- ,
+    -- 'sheriff'
+}

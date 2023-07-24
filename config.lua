e92dd91964c236437b8c9c7f8f474e813c84db14
Fezz <43589668+FBFezz@users.noreply.github.com>
2023-07-24 22:07:17 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,6 @@ Config = {}
 
 Config.AllowEveryone = true -- Enable to allow everyone to use the item / command
 Config.EnableCommand = true -- Enable or Disable the checktint command
-Config.Target = false -- Enable to use ox_target
-Config.Item = false -- Enable if you use the window tint as an item
+Config.Target = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
+Config.Item = false -- Enable if you use the window tint as an item with the target
 Config.Animation = true -- Enable to show animation
